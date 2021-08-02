1a9c1b397391d7e72f1b237d1f063248ac1f7a93
tusharmonga014 <tusharmongatt@gmail.com>
2021-08-02 19:56:44 +0530

    add page rendering and client react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
-import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import PageRender from "./PageRender";
 
 function App() {
   return (
-    <div>
-      <h1>App Running</h1>
-    </div>
+    <Router>
+      <div className='app'>
+        <Route exact path='/:page' component={PageRender} />
+        <Route exact path='/:page/:id' component={PageRender} />
+      </div>
+    </Router>
   );
 }
 
