81de984e9fba624c159d16568858e0e43cf543e8
tusharmonga014 <tusharmongatt@gmail.com>
2021-08-02 19:02:14 +0530

    add client side packages and bootstrap, font-awesome

--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ const app = express();
 /* Connects to database */
 require('./startup/db')();
 
-app.use(cors()); // url of react app
-app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server Running on ${PORT}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server Running on ${port}`);
 })
--- server/startup/db.js
+++ server/startup/db.js
@@ -5,9 +5,14 @@ module.exports = function () {
     /* Fixing all deprecationWarning of mongoDB */
     mongoose.set('useNewUrlParser', true);
     mongoose.set('useUnifiedTopology', true);
-    
+
+    /**
+     * MongoDB connection URL
+     */
+    const URL = process.env.MONGODB_URL;
+
     /* Connection to database */
-    mongoose.connect(process.env.MONGODB_URL)
+    mongoose.connect(URL)
         .then(() => console.log('Connected to MongoDB'))
         .catch((error) => console.log(`${error}, could not connect to MongoDB`));
 }
