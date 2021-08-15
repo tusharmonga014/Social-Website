00a23beeb9a264406e2bea13211dc03bca0621eb
tusharmonga014 <tusharmongatt@gmail.com>
2021-08-15 18:04:48 +0530

    deploy to heroku

--- index.js
+++ index.js
@@ -4,17 +4,29 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const routes = require('./routes');
 
+
 const app = express();
+
 
 /* Connects to database */
 require('./startup/db')();
+
 
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
+
 /* Routes */
 app.use('/api', routes);
+
+
+/* production build */
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    //  else  //   "proxy": "http://localhost:5000",
+}
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
