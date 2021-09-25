46eb206a8a26f8fa1c1fcbe8a4947221d281cf1c
tusharmonga014 <tusharmongatt@gmail.com>
2021-09-25 18:07:44 +0530

    fix refresh issue

--- index.js
+++ index.js
@@ -19,6 +19,13 @@ app.use(cookieParser());
 
 /* Routes */
 app.use('/api', routes);
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'path/to/your/index.html'), function(err) {
+      if (err) {
+        res.status(500).send(err)
+      }
+    })
+  })
 
 
 /* production build */
