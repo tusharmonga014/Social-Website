0d5922ab290707d4dc7822e6e3f3ed1851d12962
tusharmonga014 <tusharmongatt@gmail.com>
2021-10-07 01:02:19 +0530

    update loggedin

--- client/src/customRouter/PrivateRouter.js
+++ client/src/customRouter/PrivateRouter.js
@@ -9,11 +9,16 @@ const PrivateRouter = (props) => {
 
     if (isLoggedIn === 'true') {
 
-        if (auth.token)
+        if (auth.token){
+            console.log("token present");
+            console.log(isLoggedIn + " " + auth.token);
             return <Route {...props} />
-        else
+        }
+        else{
+            console.log("token not present");
+            console.log(isLoggedIn + " " + auth.token);
             return <Loading />
-
+        }
     } else {
 
         console.log(isLoggedIn);
