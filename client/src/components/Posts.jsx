4010002dfd6ff46c604e26059f35d7d1c34f5e77
tusharmonga014 <tusharmongatt@gmail.com>
2021-10-14 19:51:40 +0530

    fix folder structure renaming

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import LoadIcon from "../images/loading.gif";
-import PostCard from "./PostCard/PostCard";
+import PostCard from "./postcar/PostCard";
 import LoadMoreBtn from "./LoadMoreBtn";
 
 
